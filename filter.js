792fa4693abacea6883026d5b32002daaf994ecf
Hotpotato7420 <axejak056@gmail.com>
2025-04-27 20:20:38 +0200

    added a loading bar, checks if all number of images are added

--- filter.js
+++ filter.js
@@ -1,6 +1,14 @@
+let loading = true
 
+let defaultBackgroundColor = document.body.style.backgroundColor
 
 let map = []; // Declare map in the outer scope
+
+let totalImages = 170; // Total number of images to load
+let loadedImages = 0; // Counter for loaded images
+
+const loader = document.querySelector(".loader");
+
 
 fetch("https://ddragon.leagueoflegends.com/cdn/15.7.1/data/en_US/champion.json")
     .then((response) => response.json())
@@ -17,8 +25,24 @@ fetch("https://ddragon.leagueoflegends.com/cdn/15.7.1/data/en_US/champion.json")
             img.src = "https://ddragon.leagueoflegends.com/cdn/15.7.1/img/champion/" + element.image.full
  
             img.style.opacity = "1" // stop button from needing to be clicked twice to change opacity
+            img.setAttribute("id", element.name)
+
+            // track number of loaded images
+            img.addEventListener("load", () => {
+                loadedImages++;
+
+                // Check if all images are loaded
+                if (loadedImages === totalImages) {
+                    loading = false;
+                    document.body.style.backgroundColor = defaultBackgroundColor;
+                    loader.classList.add("loader-hidden")
+                }
+            });
+
             img.addEventListener("click", clicked)
             img.addEventListener("mousedown", (event) => {event.preventDefault()})
+
+
             
             const p = document.createElement("p")
             element.p = p
@@ -66,8 +90,9 @@ fetch("https://ddragon.leagueoflegends.com/cdn/15.7.1/data/en_US/champion.json")
                 setOpacity(1, img, p)
             } 
 
+            console.log(element.name)
+            console.log(loading)
         });
-        
 
 });
 
@@ -115,10 +140,13 @@ function customChar(previousChar) {
             const container = document.createElement("div")
 
             const img = document.createElement("img")
-            img.src = "GyroP.webp"
+            img.src = "images/GyroP.webp"
  
-            img.addEventListener("click", (event) => { changeSrc(img, "2fab9f8d92e9b982652eac015155965e.jpg", "GyroP.webp", p)})
+            img.addEventListener("click", (event) => { changeSrc(img, "images/Johnny.jpg", "images/GyroP.webp", p)})
             img.addEventListener("mousedown", (event) => {event.preventDefault()})
+            img.style.height = "122px"
+            img.style.width = "122px"
+            img.classList.add("customChar")
             
             const p = document.createElement("p")
             p.textContent = "Gyro"
@@ -130,12 +158,7 @@ function customChar(previousChar) {
     }
 
 function changeSrc(image, newSrc, originalSrc, p) {
-    console.log("original src: " + originalSrc)
-    console.log("current src: " + image.src)
-    console.log("new src: " + newSrc)
-
-    if (image.src != "http://127.0.0.1:5500/2fab9f8d92e9b982652eac015155965e.jpg") {
-        console.log("Changing src")
+    if (image.src.includes(originalSrc)) {
         image.src = newSrc
         p.textContent = "Johnny"
     }
