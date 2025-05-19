f9ef8c4073d1401b2825a1906650ec5a5bbf0326
Hotpotato7420 <axejak056@gmail.com>
2025-05-19 11:04:14 +0200

    gjort klart första help sektionen

--- help.js
+++ help.js
@@ -3,9 +3,18 @@ const darkModeToggle_light = "images/light_mode_24dp_FFFFFF_FILL0_wght400_GRAD0_
 const darkModeToggle_dark = "images/dark_mode_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg"
 
 
+
+const help1_text = document.querySelector(".help1")
+
+const help1_displayTop = document.querySelector(".displayTop")
+const help1_displayJungle = document.querySelector(".displayJungle")
+const help1_displayMiddle = document.querySelector(".displayMiddle")
+const help1_displayBottom = document.querySelector(".displayBottom")
+const help1_displaySupport = document.querySelector(".displaySupport")
+
 var roleTop = ["Aatrox", "Akshan", "Ambessa", "Aurora", "Camille", "Cho'Gath", "Darius", "Dr. Mundo", "Fiora", "Gangplank", "Garen", "Gnar", "Gragas", "Gwem", "Heimerdinger", "Illaoi", "Irelia", "Jax", "Jayce", "K'Sante", "Kayle", "Kennen", "Kled", "Malphite", "Maokai", "Mordekaiser", "Nasus", "Olaf", "Ornn", "Pantheon", "Poppy", "Quinn", "Renekton", "Riven", "Rumble", "Sett", "Shen", "Shyvana", "Singed", "Sion", "Tahm Kench", "Teemo", "Trundle", "Tryndamere", "Urgot", "Vayne", "Volibear", "Warwick", "Yorick"]
 var roleJungle = ["Ambessa", "Amumu", "Bel'Veth", "Briar", "Diana", "Ekko", "Elise", "Evelynn", "Fiddlesticks", "Graves", "Gwen", "Hecarim", "Ivern", "Jarvan IV", "Karthus", "Kayn", "Kha'Zix", "Kindred", "Lee Sin", "Lillia", "Maokai", "Master Yi", "Naafiri", "Nidalee", "Nocturne", "Nunu & Willump", "Olaf", "Poppy", "Rammus", "Rek'Sai", "Rengar", "Sejuani", "Shaco", "Shyvana", "Skarner", "Taliyah", "Talon", "Trundle", "Udyr", "Vi", "Viego", "Volibear", "Warwick", "Wukong", "Xin Zhao", "Zac"]
-var roleMid = ["Ahri", "Akali", "Akshan", "Ambessa", "Anivia", "Annie", "Aurelion Sol", "Aurora", "Azir", "Cassiopeia", "Ekko", "Fizz", "Galio", "Heimerdinger", "Hwei", "Kassadin", "Katarina", "LeBlanc", "Lissandra", "Lux", "Malzahar", "Mel", "Naafiri", "Neeko", "Orianna", "Pantheon", "Qiyana", "Quinn", "Ryze", "Singed", "Swain", "Sylas", "Syndra", "Taliyah", "Talon", "Twisted Fate", "Veigar", "Vel'Koz", "Vex", "Viktor", "Vladimir", "Xerath", "Yasuo", "Yone", "Zed", "Ziggs", "Zoe"]
+var roleMiddle = ["Ahri", "Akali", "Akshan", "Ambessa", "Anivia", "Annie", "Aurelion Sol", "Aurora", "Azir", "Cassiopeia", "Ekko", "Fizz", "Galio", "Heimerdinger", "Hwei", "Kassadin", "Katarina", "LeBlanc", "Lissandra", "Lux", "Malzahar", "Mel", "Naafiri", "Neeko", "Orianna", "Pantheon", "Qiyana", "Quinn", "Ryze", "Singed", "Swain", "Sylas", "Syndra", "Taliyah", "Talon", "Twisted Fate", "Veigar", "Vel'Koz", "Vex", "Viktor", "Vladimir", "Xerath", "Yasuo", "Yone", "Zed", "Ziggs", "Zoe"]
 var roleBottom = ["Aphelios", "Ashe", "Brand", "Caitlyn", "Corki", "Draven", "Ezreal", "Jhin", "Jinx", "Kai'Sa", "Kalista", "Kog'Maw", "Lucian", "Miss Fortune", "Nilah", "Samira", "Seraphine", "Sivir", "Smolder", "Swain", "Tristana", "Twitch", "Varus", "Vayne", "Xayah", , "Yasuo", "Zeri", "Ziggs"]
 var roleSupport = ["Alistar", "Amumu", "Bard", "Blitzcrank", "Brand", "Braum", "Janna", "Karma", "Leona", "Lulu", "Lux", "Maokai", "Milio", "Morgana", "Nami", "Nautilus", "Neeko", "Pantheon", "Poppy", "Pyke", "Rakan", "Rell", "Renata Glasc", "Senna", "Seraphine", "Sett", "Shaco", "Sona", "Soraka", "Swain", "Tahm Kench", "Taric", "Tresh", "Vel'Koz", "Xerath", "Yuumi", "Zilean", "Zyra"]
 
@@ -54,11 +63,41 @@ function switchDarkMode() {
 }
 
 
+
+for (let i = 0; i < roleTop.length; i++) {
+    help1_displayTop.innerHTML += `<p>${roleTop[i] + ""}</p>`
+}
+
+for (let i = 0; i < roleJungle.length; i++) {
+    help1_displayJungle.innerHTML += `<p>${roleJungle[i] + ""}</p>`
+}
+
+for (let i = 0; i < roleMiddle.length; i++) {
+    help1_displayMiddle.innerHTML += `<p>${roleMiddle[i] + ""}</p>`
+}
+
+for (let i = 0; i < roleBottom.length; i++) {
+    help1_displayBottom.innerHTML += `<p>${roleBottom[i]}</p>`
+}
+
+for (let i = 0; i < roleSupport.length; i++) {
+    help1_displaySupport.innerHTML += `<p>${roleSupport[i] + ""}</p>`
+}
+
+
 function openClass(number) {
     let openHelp = document.querySelector(".help" + number)
     console.log(openHelp.style.display)
+    console.log(openHelp.style.display)
+    console.log(openHelp.style.display)
+    console.log(openHelp.style.display)
+    console.log(openHelp.style.display)
+    if (!openHelp.style.display) {
+        openHelp.style.display = "none"
+    }
     if (openHelp.style.display == "block") {
-            openHelp.style.display = "none"    
+            openHelp.style.display = "none"
+            console.log("hiding")
     }
     else {
         openHelp.style.display = "block"
